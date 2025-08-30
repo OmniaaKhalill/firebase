8e2cd2b4e2e5458f87c26960ef5531dbbaeec25c
Omnia Khalil <omniaakhalill@example.com>
2025-08-30 21:12:27 +0300

    Remove Hosting Files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,9 +15,9 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    // provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    // provideAuth(() => getAuth()),
-    // provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
  
  
   ]
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,8 +1,14 @@
 export const environment ={
-    production:false
-
+    production:false,
+ firebaseConfig :{
+  apiKey: "AIzaSyAi51fGr093K8iE31itjDbODIWHEz3NWWc",
+  authDomain: "workspace-f8a26.firebaseapp.com",
+  projectId: "workspace-f8a26",
+  storageBucket: "workspace-f8a26.firebasestorage.app",
+  messagingSenderId: "106636076864",
+  appId: "1:106636076864:web:53da2ada62a3a0ca23fca5"
 }
 
 
    
-
+}
